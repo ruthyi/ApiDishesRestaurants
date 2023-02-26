27d0f8b3dac8da6d34d576f42e961c01793dbf16
ruthyi <jbruthlizcano@gmail.com>
2023-02-26 15:45:46 -0500

    add login and token

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const router = express.Router()
-// const checkAuth = require('../middleware/auth')
-// const checkRoleAuth = require('../middleware/roleAuth')
- const {
+const checkAuth = require('../middleware/auth')
+const checkRoleAuth = require('../middleware/roleAuth')
+const {
   getItems,
-   getItem,
-    createItem,
-    deleteItem,
-    updateItem } = require('../controller/userController')
-
-router.get('/', getItems)
+  getItem,
+  createItem,
+  deleteItem,
+  updateItem } = require('../controller/userController')
+ // checkAuth, checkRoleAuth(['admin']),
+router.get('/',  getItems)
 
 router.get('/:id', getItem)
 
 //TODO: Donde recibimos data
 router.post('/', createItem)
 
-router.patch('/:id', updateItem)
+router.put('/:id', updateItem)
 
-router.delete('/:id', deleteItem)
+router.delete('/', deleteItem)
 
 
 module.exports = router
